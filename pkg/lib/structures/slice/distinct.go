71934cbc026f7bbc946f385ec94665e91fe2779f
juricake <jurica.kenda@axilis.com>
2023-10-19 13:20:29 +0200

    init slice helper

---
+++ pkg/lib/structures/slice/distinct.go
@@ -0,0 +1,39 @@
+package slice
+
+import (
+	"github.com/juricake/goodies/pkg/lib/structures/set"
+)
+
+// Distinct slice data structure.
+type Distinct[V comparable] struct {
+	store  []V
+	filter *set.Set[V]
+}
+
+// NewDistinct slice constructor.
+func NewDistinct[V comparable](values ...V) *Distinct[V] {
+	d := &Distinct[V]{store: make([]V, 0), filter: set.New[V]()}
+	d.Append(values...)
+	return d
+}
+
+// Append a single or multiple values inside the slice.
+func (s *Distinct[V]) Append(values ...V) {
+	for _, value := range values {
+		if s.filter.Contains(value) {
+			continue
+		}
+		s.store = append(s.store, value)
+		s.filter.Put(value)
+	}
+}
+
+// GetAll values from the slice.
+func (s *Distinct[V]) GetAll() []V {
+	return s.store
+}
+
+// Size of the slice.
+func (s *Distinct[V]) Size() int {
+	return len(s.store)
+}
