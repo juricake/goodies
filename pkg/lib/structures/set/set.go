2544144b1969912c04d9c6dd02ac1d8b5c805492
juricake <jurica.kenda@axilis.com>
2023-10-19 13:20:07 +0200

    init set helper

---
+++ pkg/lib/structures/set/set.go
@@ -0,0 +1,35 @@
+package set
+
+type Set[V comparable] struct {
+	store map[V]struct{}
+}
+
+// New constructor.
+func New[V comparable](values ...V) *Set[V] {
+	s := &Set[V]{store: make(map[V]struct{}, 0)}
+	s.Put(values...)
+	return s
+}
+
+// Put value inside the set.
+func (s *Set[V]) Put(values ...V) {
+	for _, value := range values {
+		s.store[value] = struct{}{}
+	}
+}
+
+// Del value from the set.
+func (s *Set[V]) Del(value V) {
+	delete(s.store, value)
+}
+
+// Contains returns true if the set contains the value, false otherwise.
+func (s *Set[V]) Contains(value V) bool {
+	_, exists := s.store[value]
+	return exists
+}
+
+// Size of the set.
+func (s *Set[V]) Size() int {
+	return len(s.store)
+}
